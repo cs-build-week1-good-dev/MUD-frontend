a1e624236f7327f2566dae98fa85e872a1a3f1ef
Josh <Kersting.Josh@Gmail.com>
2019-07-29 13:36:24 -0400

    initial commit, scaffold set

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import { Route } from "react-router-dom";
+// import PrivateRoute from './components/PrivateRouter';
+
+export default function(props) {
+  return (
+    <div className="App">
+      <h1>Isssa App</h1>
+    </div>
+  );
+}
---
+++ src/actions/loginActions.js
@@ -0,0 +1,10 @@
+// LOGIN_USER, LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE
+export const LOGIN_USER = "LOGIN_USER";
+export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
+export const LOGIN_USER_FAILURE = "LOGIN_USER_FAILURE";
+
+export const doLogin = () => dispatch => {
+  dispatch({
+    type: LOGIN_USER
+  });
+};
