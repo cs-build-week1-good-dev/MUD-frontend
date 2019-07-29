98269077adc0f83a25ce7d79294383eb68a87e76
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-07-29 13:06:51 -0500

    file path changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import loginReducer from "./projectsReducer";
+import loginReducer from "./loginReducer";
 
 export default combineReducers({
   login: loginReducer
