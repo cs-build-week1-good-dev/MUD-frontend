36144cab4fe03de3e40256d1e26c14c5887b06da
Josh <Kersting.Josh@Gmail.com>
2019-07-29 14:02:10 -0400

    added dependencies

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import thunk  from "redux-thunk"
+import thunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
 
-import './index.css';
-import rootReducer from './reducers'
-import App from './App';
-
+import "./index.css";
+import rootReducer from "./reducers";
+import App from "./App";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunk))
-)
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
