5a01a223dba16993d83e925ab9607ee53ff6e858
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-08-01 13:57:32 -0500

    messaging configuration

--- src/actions/Push.js
+++ src/actions/Push.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { AUTH_TOKEN } from "../config";
 export const PUSHER_SEND_START = "PUSHER_SEND_START";
 export const PUSHER_SEND_SUCCESS = "PUSHER_SEND_SUCCESS";
 export const PUSHER_SEND_FAILURE = "PUSHER_SEND_FAILURE";
@@ -11,7 +12,7 @@ export const pushMessage = message => dispatch => {
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
-        Authorization: `Token 3bee58df292ce4c461076e022acc9e4ecfdd28b7`
+        Authorization: `Token ${localStorage.getItem(AUTH_TOKEN)}`
       }
     })
     .then(res => {
